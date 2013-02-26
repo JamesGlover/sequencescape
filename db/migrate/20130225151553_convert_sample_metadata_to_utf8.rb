e35e7e51948b6613d2d63cdcf686e318054aacf2
James Glover <jg16@sanger.ac.uk>
2013-02-26 10:10:15 +0000

    [#45065527] Ensure columns are converted with table

--- db/migrate/20130225151553_convert_sample_metadata_to_utf8.rb
+++ db/migrate/20130225151553_convert_sample_metadata_to_utf8.rb
@@ -1,13 +1,13 @@
 class ConvertSampleMetadataToUtf8 < ActiveRecord::Migration
   def self.up
     ActiveRecord::Base.transaction do
-      execute('ALTER TABLE sample_metadata CHARACTER SET utf8 COLLATE utf8_unicode_ci;')
+      execute('ALTER TABLE sample_metadata CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;')
     end
   end
 
   def self.down
     ActiveRecord::Base.transaction do
-      execute('ALTER TABLE sample_metadata CHARACTER SET latin1 COLLATE latin1_swedish_ci;')
+      execute('ALTER TABLE sample_metadata CONVERT TO CHARACTER SET latin1 COLLATE latin1_swedish_ci;')
     end
   end
 end
