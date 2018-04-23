0ff8fed89981a966047d8e7a5aa6d56e85cea383
James Glover <james.glover@sanger.ac.uk>
2018-04-23 09:17:16 +0100

    Update infinium barcode in views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420153000) do
+ActiveRecord::Schema.define(version: 20180423080956) do
 
   create_table "aker_containers", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "barcode"
